c84fb0de7811222f864c6e18938f55a2ba7678f3
Emilie Allen <emilie.allen@hotmail.co.uk>
2022-03-26 17:08:05 +0000

    fixing navbar and adding about

--- client/src/App.js
+++ client/src/App.js
@@ -7,26 +7,27 @@ import Navbar from "./components/common/Navbar"
 import About from "./components/common/About"
 // import AllRecipes from "./components/AllRecipes"
 // import { useState } from "react"
+<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+
+
 
 function App() {
 
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
     <Router>
         <Routes>
-        <Route path="/" element={<Navbar />}>
-        <Route index element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/about" element={<About />} />
           </Route>
         </Routes>
     </Router>
     </div>
   )
 }
-
-
 
 export default App
--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -5,28 +5,14 @@ import React from "react";
 function Navbar() {
     return (
         <nav>
-            <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
-                <div class="container">
-                    <a href="#" class="navbar-brand">Simply Good Food</a>
-
-                    <button
-                        class="navbar-toggler"
-                        type="button"
-                    >
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navmenu">
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item">
-                                <a href="#all-recipes" class="nav-link">All Recipes</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#about" class="nav-link">About</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#menuideas" class="nav-link">Menu Ideas</a>
-                            </li>
+            <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
+                <div className="container">
+                    <Link to="/" className="navbar-brand">Simply Good Food</Link>
+                    <div className="collapse navbar-collapse" id="navmenu">
+                        <ul className="navbar-nav ms-auto">
+                            <Link to="/all-recipes" className="nav-link">All Recipes</Link>
+                            <Link to="/about" className="nav-link">About</Link>
+                            <Link to="/menuideas" className="nav-link">Menu Ideas</Link>
                         </ul>
                     </div>
                 </div>
