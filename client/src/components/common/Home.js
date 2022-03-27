dd608ff1056d9702986de412b07a6d98d78f65ee
Rachel Tipton <RACHELMTIPTON@GMAIL.COM>
2022-03-27 17:45:45 +0200

    worked on styling for RecipeCard, MenuPage, All-Recipes Page, and the RecipePage

--- client/src/components/common/Home.js
+++ client/src/components/common/Home.js
@@ -1,24 +1,31 @@
 //this is our home page that shows nine different random recipe cards and the featured recipe
 import React from "react"
 import RecipeCard from "../recipe-components/RecipeCard"
+import { Link } from "react-router-dom"
 
 function Home() {
 
     return (
         <>
-            <div classNameName="container">
-                    <div className="row align-items-start">
-                        <div className="col">
-                        <RecipeCard />
+            <section className="bg-light text-dark text-start">
+                <div className="container pt-5">
+                    <div className="d-md-flex align-items-center justify-content-around">
+                        <div>
+                            <h1>Simply Good Food</h1>
+                            <div><h2 className="lead py-5">Just two web developers bringing you their favorite healthy recipes without the hastle of scrolling through wordy blogs.</h2></div>
+                            <Link to="/all-recipes"><button type="button" className="btn btn-outline-dark">Take Me to All Recipes</button></Link>
                         </div>
-                        <div className="col">
-                        <RecipeCard />
-                        </div>
-                        <div className="col">
-                        <RecipeCard />
-                        </div>
+                        <img className="img-fluid py-5 px-5 d-sm-block" height="500" width="300" src="https://i.ibb.co/hgD0tv1/HEALTHY-Mango-Green-Salad-with-Creamy-Peanut-Dressing-15-min-9-ingredients-SO-delicious-salad-vegan.jpg" alt="homepage food" />
                     </div>
-                    <div className="row align-items-center">
+                   
+                </div>
+            </section>
+            <div className="container pt-5 pb-5">
+                <h1 className="text-align-center pb-5">Our Featured Recipes</h1>
+                    <div className="row align-items-start">
+                    <div className="row g-5 align-items-center justify-conent-center w-50">
+                        <RecipeCard />
+                    </div>
                 </div>  
                 </div>
         </>          
--- client/src/components/recipe-components/AllRecipes.js
+++ client/src/components/recipe-components/AllRecipes.js
@@ -1,1 +1,28 @@
 //this page shows all recipes 
+import RecipeCard from "./RecipeCard"
+import { Link } from "react-router-dom"
+function AllRecipes() {
+    return (
+        <section className="bg-light text-dark text-start">
+            <div className="container pt-5">
+                <div className="d-md-flex align-items-center justify-content-around">
+                    <div>
+                        <h1>Simply Good Recipes</h1>
+                        <div><h2 className="lead py-5">On this page you can find all of our recipes. There aren't that many, but if you can go to our Menu Ideas page if you'd like to check out our recipes by category.</h2></div>
+                        <Link to="/menuideas"><button type="button" className="btn btn-outline-dark">Take Me to Menu Ideas</button></Link>
+                    </div>
+                    <img className="img-fluid py-5 px-5 d-sm-block" height="500" width="300" src="https://i.ibb.co/hgD0tv1/HEALTHY-Mango-Green-Salad-with-Creamy-Peanut-Dressing-15-min-9-ingredients-SO-delicious-salad-vegan.jpg" alt="homepage food" />
+                </div>
+                <div className="container align-items-center justify-content-around pt-5 pb-5">
+                <h1 className="text-align-center pb-5">Have a scroll through all our recipes.</h1>
+                    <div className="row g-5 align-items-center w-50">
+                        <RecipeCard />
+                    </div>
+                </div>
+                </div>
+        </section>
+       
+    )
+}
+
+export default AllRecipes
