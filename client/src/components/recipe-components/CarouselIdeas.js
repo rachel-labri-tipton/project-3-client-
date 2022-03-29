a4e47ffe70691916a03a157202aef697964685dd
Rachel Tipton <RACHELMTIPTON@GMAIL.COM>
2022-03-29 22:48:25 +0200

    tryting to get a carousel working

---
+++ client/src/components/recipe-components/CarouselIdeas.js
@@ -0,0 +1,24 @@
+
+function CarouselIdeas() {
+    return (
+        <>
+        <section className="container p-5">
+        <div className="carousel slide" data-bs-ride="carousel">
+            <div id="carousel-inner">
+                <div className="carousel-item active">
+                            <img width="300" height="500"src="https://i.ibb.co/kczrxYz/INCREDIBLE-5-Ingredient-Banana-Egg-Pancakes-Fluffy-dairy-free-grain-free-no-sugar-added-SO-satisfyin.jpg" className="d-block w-100" alt="pancakes" />
+                    <div className="carousel-item">
+                        <img src="https://i.ibb.co/Wt3c7mw/Instant-Pot-Banana-Bread-Steel-Cut-Oats-Fast-easy-BIG-banana-bread-flavor-oats-bananabread-instantpo.jpg" className="d-block w-100" alt="oatmeal" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://i.ibb.co/s2f6Z58/THE-BEST-AMAZING-Peanut-Butter-Overnight-Oats-Just-5-ingredients-5-minutes-prep-and-SO-delicious-veg.jpg" className="d-block w-100" alt="PB overnight oats" />
+                    </div>
+                </div>
+            </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default CarouselIdeas
--- client/src/components/recipe-components/IdeasAndSuggestions.js
+++ client/src/components/recipe-components/IdeasAndSuggestions.js
@@ -1,8 +1,10 @@
 //this page presents five different meal types: Breakfast, Lunch, Dinner, Snacks, Desserts
 //this page also contains the link to the Detox, which is a stretch goal
 import { Link } from "react-router-dom"
+import CarouselIdeas from "./CarouselIdeas"
 function IdeasAndSuggestions() {
     return (
+        <>
             <section className="bg-light text-dark text-start">
             <div className="container pt-5">
                 <div className="d-md-flex align-items-center justify-content-around">
@@ -14,28 +16,9 @@ function IdeasAndSuggestions() {
                     <img className="img-fluid py-5 px-5 d-sm-block" height="500" width="300" src="https://i.ibb.co/Fsx9BRW/NOURISHING-Black-Bean-Buddha-Bowl-with-Rice-Greens-Kraut-and-Lemon-Ginger-Tahini-Sauce-Simple-BIG-fl.jpg" alt="homepage food" />
                 </div>
             </div>
-                <div id="slides" className="carousel slide" data-bs-ride="carousel">
-                    <div id="carousel-inner">
-                        <div className="carousel-item active">
-                        <img src="https://i.ibb.co/kczrxYz/INCREDIBLE-5-Ingredient-Banana-Egg-Pancakes-Fluffy-dairy-free-grain-free-no-sugar-added-SO-satisfyin.jpg" className="d-block w-50" alt="pancakes"/>
-                        <div className="carousel-item">
-                        <img src="https://i.ibb.co/Wt3c7mw/Instant-Pot-Banana-Bread-Steel-Cut-Oats-Fast-easy-BIG-banana-bread-flavor-oats-bananabread-instantpo.jpg" className="d-block w-50" alt="oatmeal"/>
-                            </div>
-                        <div className="carousel-item">
-                        <img src="https://i.ibb.co/s2f6Z58/THE-BEST-AMAZING-Peanut-Butter-Overnight-Oats-Just-5-ingredients-5-minutes-prep-and-SO-delicious-veg.jpg" className="d-block w-50" alt="PB overnight oats" />
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#slides" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span>Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#slides" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span>Next</span>
-                        </button>
-                    </div>
-                </div>
-                </div>
-                    </section>)
+        </section>
+         <CarouselIdeas />
+        </>)
 
 }
 
